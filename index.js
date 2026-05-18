f289ecf5da2234191b27c953f90450bed4f8eded
sabbir <sabbirhossain1177034@gmail.com>
2026-05-18 11:44:13 +0600

    Setup MongoDB connection and configure database integration

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,9 +20,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db("ideavault");
+    const ideaVaultCollection = db.collection("ideas");
+
+    app.get("/idea", async (req, res) => {
+      const result = await ideaVaultCollection.find().toArray();
+      res.json(result);
+    });
+
+    app.post("/idea", async (req, res) => {
+      const ideaData = req.body;
+      console.log(ideaData);
+      const result = await ideaVaultCollection.insertOne(ideaData);
+      res.json(result);
+    });
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
